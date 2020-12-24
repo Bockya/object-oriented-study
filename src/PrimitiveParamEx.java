406a851ca56ff5319efc2b202f44ab57929ebac5
Bockya <rhk3562@naver.com>
2020-12-25 08:45:52 +0900

    Feat PrimitiveParamEx : 3.8 기본형 매개변수

---
+++ src/PrimitiveParamEx.java
@@ -0,0 +1,16 @@
+class PrimitiveParamEx {
+    public static void main(String[] args) {
+        Data d = new Data();
+        d.x = 10;
+        System.out.println("main(): x = " + d.x);
+
+        change();
+        System.out.println("After change(d.x)");
+        System.out.println("main(): x = " + d.x);
+    }
+
+    private static void change() {
+        int x = 1000;
+        System.out.println("change(): x = " + x);
+    }
+}
